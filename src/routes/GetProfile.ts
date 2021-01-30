53bc80171db4d3db50a61e8b7ad81b6662f79351
Alexis Tan <alexis@sen-dev.com>
2021-01-30 16:16:13 +0100

    Add check to make sure user is a member or profiles

--- src/routes/GetProfile.ts
+++ src/routes/GetProfile.ts
@@ -17,6 +17,10 @@ export default asyncWrap(async (request: Request, response: Response) => {
   const minifiedUuid = uuid.replace(/-/g, '')
 
   for (let profile of profiles.data.profiles) {
+    if (!profile.members.hasOwnProperty(minifiedUuid)) {
+      continue
+    }
+
     result.push({
       id: profile.profile_id,
       name: profile.cute_name,
@@ -24,6 +28,13 @@ export default asyncWrap(async (request: Request, response: Response) => {
         skills: SkillsGenerator.build(minifiedUuid, profile.members),
       },
     })
+  }
+
+  // Throws a 404 if the user doesn't have any SkyBlock profiles, this step can be
+  // reached if the user is invited to a profile so it shows up as a SkyBlock
+  // profile in the API, but they haven't accepted the invitation yet.
+  if (result.length == 0) {
+    throw new HttpException(404, `Found no SkyBlock profiles for a user with a UUID of '${uuid}'`)
   }
 
   return response.status(200).json({
