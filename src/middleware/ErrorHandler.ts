a07ff461759337a08106363d4e131f07d9bf708b
Alexis Tan <alexis@sen-dev.com>
2021-02-01 16:08:14 +0100

    Add better error handling when Hypixels API responses with an error

--- src/middleware/ErrorHandler.ts
+++ src/middleware/ErrorHandler.ts
@@ -6,12 +6,26 @@ import HttpException from '../exceptions/HttpException'
  * Handles errors thrown within the routes and returns
  * more sane error responses back to the user.
  */
-export default (error: Error, _: Request, response: Response, __: any) => {
+export default (error: any, _: Request, response: Response, __: any) => {
   if (error instanceof HttpException) {
     return response.status(error.statusCode).json({
       status: error.statusCode,
       reason: error.message,
     })
+  } else if (error.hasOwnProperty('response')) {
+    switch (error.response.status) {
+      case 403:
+        return createJsonResponse(response, 403, 'Invalid Hypixel API token provided')
+
+      case 404:
+        return createJsonResponse(response, 404, 'The requested resource does not exist')
+
+      case 429:
+        return createJsonResponse(response, 429, 'You have hit the rate-limit, please slow down your requests')
+
+      case 503:
+        return createJsonResponse(response, 503, 'Hypixels API is currently in maintenance mode, try again later')
+    }
   }
 
   const jsonResponse: any = {
@@ -24,4 +38,18 @@ export default (error: Error, _: Request, response: Response, __: any) => {
   }
 
   return response.status(500).json(jsonResponse)
+}
+
+/**
+ * Creates a JSON response with the given status code and reason.
+ *
+ * @param response The Express response for the current request
+ * @param statusCode The status code for the response
+ * @param reason The reason describing what went wrong
+ */
+function createJsonResponse(response: Response, statusCode: number, reason: string) {
+  return response.status(statusCode).json({
+    status: statusCode,
+    reason: reason,
+  })
 }
