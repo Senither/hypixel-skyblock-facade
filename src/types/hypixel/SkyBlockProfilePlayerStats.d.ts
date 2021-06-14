7cbaf1c5cdc5a78ea81473cdecf8dff48ab8502a
Calvahar <58914729+Calvahar@users.noreply.github.com>
2021-06-15 00:36:16 +0200

    Update SkyBlockProfilePlayerStats.d.ts

--- src/types/hypixel/SkyBlockProfilePlayerStats.d.ts
+++ src/types/hypixel/SkyBlockProfilePlayerStats.d.ts
@@ -8,7 +8,7 @@ export default interface SkyBlockProfilePlayerStats {
     time: number
     date: Date
   }
-  fairy_souls_collected: number
+  fairy_souls: number
   weight: number
   weight_overflow: number
   skills: SkyBlockSkillGroupResponse | null
